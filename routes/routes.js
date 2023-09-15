06b5c8a9d3e10f59de274995ef8e949fa71f8216
Rohanmrao <rohanmaheshrao10@gmail.com>
2023-09-15 17:51:05 +0530

    removed routes

--- routes/routes.js
+++ routes/routes.js
@@ -4,13 +4,8 @@ const mongoose = require('mongoose');
 const visitor_model = require('../models/schema');
 const path = require('path');
 
-router.get();
-router.post();
-router.patch();
-router.delete();
 
 //middleware core
-
 async function get_vis(req,res,next) {
 
     let vis;
